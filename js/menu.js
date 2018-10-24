71e4bba42dbcc6489dce4bf35dd256ab4c63e05d
karlshaver <karl@meshfresh.com>
2018-10-24 17:21:12 -0400

    Port changes from T1 - Menu function, conditional hover

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,8 @@
 jQuery(document).ready(function($){
    $('#mobileMenuTrigger').click(function(){
       $('.main-navigation ul').slideToggle();
+      $('.main-navigation ul li > a').on("click", function(){
+         $('.main-navigation ul').slideUp();
+      });
    });
 });
